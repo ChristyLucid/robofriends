833c95c63827af67f855c0224cb4e0fa17f74de8
Christy Cassidy <cassidyc@lucidltd.com>
2020-07-19 02:17:32 +0100

    Initialise

---
+++ src/containers/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+
+    console.log("componentDidMount");
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+
+    const filterdRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+
+    console.log("render");
+    // console.log(filterdRobots);
+
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filterdRobots} />;
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+// const App = () => {
+//   return (
+//     <div className="tc">
+//       <h1>RoboFriends</h1>
+//       <SearchBox />
+//       <CardList robots={robots} />;
+//     </div>
+//   );
+// };
+
+export default App;
